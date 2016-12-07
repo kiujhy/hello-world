f6db50829c796509119f14a9ab3b844a2452e58c
kiujhy <li20li20p@gmail.com>
2016-12-07 10:09:36 +0200

    ft_putstr

---
+++ C/September/ft_putstr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+
+    int i;
+
+    i = 0;
+    
+    while(str[i] != '\0')
+    {
+        ft_putchar(str[i]);
+        i++;
+       
+    }
+}
+int main(void)
+{
+    char str[] = "Hello, World!!!";
+    
+    ft_putstr(&str[0]);
+    return(0);
+}
